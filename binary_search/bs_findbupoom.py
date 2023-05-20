15ccfacb370a3ca6828946c0fa6709f32692700b
JosephSuh <josephsuh@seohoui-MacBookPro.local>
2023-05-20 11:12:20 +0900

    Add solution using recurrsion (findbupoom problem)

--- binary_search/bs_findbupoom.py
+++ binary_search/bs_findbupoom.py
@@ -12,6 +12,20 @@ def binary_search(arrs, target, start, end):
     return False
 
 
+def binary_search2(arrs, target, start, end):
+    if start > end:
+        return False
+
+    mid = (start + end) // 2
+
+    if arrs[mid] == target:
+        return True
+    elif arrs[mid] > target:
+        return binary_search2(arrs, target, start, mid - 1)
+    else:
+        return binary_search2(arrs, target, mid + 1, end)
+
+
 if __name__ == "__main__":
     N = int(input())
     total_arrs = list(map(int, input().strip().split()))
@@ -22,7 +36,7 @@ if __name__ == "__main__":
     res = []
     for to_find in to_find_arrs:
 
-        if binary_search(total_arrs, to_find, 0, len(total_arrs) - 1):
+        if binary_search2(total_arrs, to_find, 0, len(total_arrs) - 1):
             res.append("yes")
         else:
             res.append("no")
